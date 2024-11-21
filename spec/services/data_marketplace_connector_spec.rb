26bb594bfdddc6dd3b28eda258808dc46f5546b6
Rob Nichols <robert.nichols@digital.cabinet-office.gov.uk>
2024-11-21 14:23:40 +0000

    Add data marketplace connector

---
+++ spec/services/data_marketplace_connector_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe DataMarketplaceConnector, type: :service do
+  describe ".create" do
+    let(:token) { SecureRandom.uuid }
+    let(:record) { create :record }
+
+    before do
+      stub_request(:post, "https://apitest.datamarketplace.gov.uk/v1/clientauth/get-token")
+         .with(
+           body: Rails.application.credentials.dm_api.to_json,
+           headers: { "Content-Type" => "application/json" },
+         )
+         .to_return(status: 200, body: { token: }.to_json)
+
+      stub_request(:post, "https://apitest.datamarketplace.gov.uk/v1/datasets")
+        .with(
+          body: record.metadata.to_json,
+          headers: { "Content-Type" => "application/json" },
+        )
+        .to_return(status: 200, body: "", headers: {})
+    end
+
+    it "returns success" do
+      expect(described_class.create(record)).to be_success
+    end
+  end
+end
