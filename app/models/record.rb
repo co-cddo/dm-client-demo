bd2c73a977d8e111b565fdbf97f20920ddf41f5c
Rob Nichols <robert.nichols@digital.cabinet-office.gov.uk>
2024-11-21 08:53:56 +0000

    Tidy up Record views
    - Ensure metadata saved as JSON

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,3 +1,14 @@
 class Record < ApplicationRecord
-  validates :name, presence: true
+  before_validation :json_parse_metadata
+
+  validates :name, :metadata, presence: true
+
+  def json_parse_metadata
+    return if metadata.blank?
+    return if metadata.is_a?(Hash)
+
+    self.metadata = JSON.parse(metadata)
+  rescue JSON::ParserError => e
+    errors.add :metadata, e.message
+  end
 end
