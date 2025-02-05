746b0d402856e9eaed296585c0680437a2b774e4
Rob Nichols <robert.nichols@digital.cabinet-office.gov.uk>
2025-02-05 15:46:29 +0000

    Add OS Record Processor
    Takes OS DCAT record (in XML format) and generates DM metadata from it

---
+++ app/services/os/record_processor.rb
@@ -0,0 +1,41 @@
+class OS::RecordProcessor
+  MAPPING_FILE = File.expand_path('os_to_dm_mapping.yml', __dir__)
+
+  attr_reader :source_data
+
+  def initialize(source_data:)
+    @source_data = source_data
+  end
+
+  def metadata
+    matadata ||= extract_metadata_from_source_data(mapping)
+  end
+
+  private
+
+  def extract_metadata_from_source_data(map_segment)
+   map_segment.each_with_object({}) do |(json_key, xml_path), output|
+      next if xml_path.length < 1
+      output[json_key] = if xml_path.is_a?(Array)
+        xml_path.collect {|v| extract_metadata_from_source_data(v)}.flatten
+      else
+        results = extract_from_source_doc(xml_path)
+        results = results.first if results.length == 1
+        results
+      end
+    end
+  end
+
+  def mapping
+    @mapping ||= YAML.load_file(MAPPING_FILE)
+  end
+
+  def source_doc
+    @source_doc ||= Nokogiri::XML(source_data)
+  end
+
+  def extract_from_source_doc(path)
+    target = path.include?("@") ? :values : :text
+    source_doc.xpath(path).collect(&target).flatten
+  end
+end
